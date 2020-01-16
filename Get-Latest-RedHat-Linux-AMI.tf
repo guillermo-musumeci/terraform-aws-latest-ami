bbf9608bf277f598b805d092c9b75f5e0aeecd00
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2020-01-16 15:44:32 +0100

    Initial commit

--- Get-Latest-RedHat-Linux-AMI.tf
+++ Get-Latest-RedHat-Linux-AMI.tf
@@ -4,7 +4,7 @@
 # Get latest Red Hat Enterprise Linux 6.x AMI
 data "aws_ami" "redhat-linux-6" {
   most_recent = true
-  owners = ["309956199498"]
+  owners      = ["309956199498"]
   filter {
     name   = "name"
     values = ["RHEL-6.*"]
@@ -14,7 +14,7 @@ data "aws_ami" "redhat-linux-6" {
 # Get latest Red Hat Enterprise Linux 7.x AMI
 data "aws_ami" "redhat-linux-7" {
   most_recent = true
-  owners = ["309956199498"]
+  owners      = ["309956199498"]
   filter {
     name   = "name"
     values = ["RHEL-7.*"]
@@ -24,7 +24,7 @@ data "aws_ami" "redhat-linux-7" {
 # Get latest Red Hat Enterprise Linux 8.x AMI
 data "aws_ami" "redhat-linux-8" {
   most_recent = true
-  owners = ["309956199498"]
+  owners      = ["309956199498"]
   filter {
     name   = "name"
     values = ["RHEL-8.*"]

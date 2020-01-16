67e16b33ff92941b94ef348bb09187e119e52538
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2020-01-16 15:51:01 +0100

    Initial commit

--- Get-Latest-SuSE-Linux-AMI.tf
+++ Get-Latest-SuSE-Linux-AMI.tf
@@ -4,20 +4,21 @@
 # Get latest SUSE Linux Enterprise Server 12 SP3 AMI
 data "aws_ami" "suse-linux-12-sp3" {
   most_recent = true
+  owners      = ["013907871322"] # Amazon
   filter {
-    name = "name"
+    name   = "name"
     values = ["suse-sles-12-sp3-v*-hvm-ssd-x86_64"]
   }
   filter {
-    name = "virtualization-type"
+    name   = "virtualization-type"
     values = ["hvm"]
   }
-  owners = ["013907871322"] # Amazon
 }
 
 # Get latest SUSE Linux Enterprise Server 12 SP4 AMI
 data "aws_ami" "suse-linux-12-sp4" {
   most_recent = true
+  owners      = ["013907871322"] # Amazon
   filter {
     name = "name"
     values = ["suse-sles-12-sp4-v*-hvm-ssd-x86_64"]
@@ -26,19 +27,46 @@ data "aws_ami" "suse-linux-12-sp4" {
     name = "virtualization-type"
     values = ["hvm"]
   }
-  owners = ["013907871322"] # Amazon
+}
+
+# Get latest SUSE Linux Enterprise Server 12 SP5 AMI
+data "aws_ami" "suse-linux-12-sp5" {
+  most_recent = true
+  owners      = ["013907871322"] # Amazon
+  filter {
+    name   = "name"
+    values = ["suse-sles-12-sp5-v*-hvm-ssd-x86_64"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
 # Get latest SUSE Linux Enterprise Server 15 AMI
 data "aws_ami" "suse-linux-15" {
   most_recent = true
+  owners      = ["013907871322"] # Amazon
   filter {
-    name = "name"
+    name   = "name"
     values = ["suse-sles-15-v*-hvm-ssd-x86_64"]
   }
   filter {
-    name = "virtualization-type"
+    name   = "virtualization-type"
     values = ["hvm"]
   }
-  owners = ["013907871322"] # Amazon
+}
+
+# Get latest SUSE Linux Enterprise Server 15 SP1 AMI
+data "aws_ami" "suse-linux-15-sp1" {
+  most_recent = true
+  owners      = ["013907871322"] # Amazon
+  filter {
+    name   = "name"
+    values = ["suse-sles-15-sp1-v*-hvm-ssd-x86_64"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }

74effe71592b195907bb843b6b95bddac0d667e4
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2020-01-16 15:41:11 +0100

    Initial commit

--- Get-Latest-Debian-AMI.tf
+++ Get-Latest-Debian-AMI.tf
@@ -4,29 +4,29 @@
 # Get latest Debian Linux Jessie 8 AMI
 data "aws_ami" "debian-linux-8" {
   most_recent = true
+  owners      = ["679593333241"]
   filter {
     name   = "name"
     values = ["debian-jessie*"]
   }
-  owners = ["679593333241"]
 }
 
 # Get latest Debian Linux Stretch 9 AMI
 data "aws_ami" "debian-linux-9" {
   most_recent = true
+  owners      = ["679593333241"]
   filter {
     name   = "name"
     values = ["debian-stretch*"]
   }
-  owners = ["679593333241"]
 }
 
 # Get latest Debian Linux 10 AMI
 data "aws_ami" "debian-linux-10" {
   most_recent = true
+  owners      = ["679593333241"]
   filter {
     name   = "name"
     values = ["debian-10*"]
   }
-  owners = ["136693071363"]
 }
